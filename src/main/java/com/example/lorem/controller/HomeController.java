fc6156177e2e06495b3294d927bc1ed55b308dad
5ooyeon <131759990+5ooyeon@users.noreply.github.com>
2023-04-27 17:37:01 +0900

    Update HomeController.java
    로그인 세션 유지

--- src/main/java/com/example/lorem/controller/HomeController.java
+++ src/main/java/com/example/lorem/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.example.lorem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        boolean loggedIn = (session.getAttribute("loggedIn") != null && (boolean) session.getAttribute("loggedIn"));
+        model.addAttribute("memberEmail", session.getAttribute("memberEmail"));
+        model.addAttribute("loggedIn", loggedIn);
         return "index";
     }
 }
