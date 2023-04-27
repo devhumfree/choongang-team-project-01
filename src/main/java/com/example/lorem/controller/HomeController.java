cd1b0818d7f7d5f868ee4caedbaca30671c745e4
5ooyeon <131759990+5ooyeon@users.noreply.github.com>
2023-04-27 10:39:21 +0900

    Update HomeController.java
    로그인/ 로그아웃

--- src/main/java/com/example/lorem/controller/HomeController.java
+++ src/main/java/com/example/lorem/controller/HomeController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String index() {
+    public String index(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        boolean loggedIn = (session.getAttribute("loggedIn") != null && (boolean) session.getAttribute("loggedIn"));
+        model.addAttribute("loggedIn", loggedIn);
         return "index";
     }
 }
